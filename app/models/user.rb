c802e8077bdef54ab062dd37728bd541a913e555
Igor <igorok.babenko@gmail.com>
2021-12-21 22:57:51 +0300

    Added count of autolike follows into user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :autolike_follower_follows, foreign_key: :autolike_following_id, class_name: "Autolike"
   has_many :autolike_followers, through: :autolike_follower_follows, source: :autolike_follower
 
-  has_many :autolike_following_follows, foreign_key: :follower_id, class_name: "Autolike"
+  has_many :autolike_following_follows, foreign_key: :autolike_follower_id, class_name: "Autolike"
   has_many :autolike_followings, through: :autolike_following_follows, source: :autolike_following
 
   acts_as_voter
